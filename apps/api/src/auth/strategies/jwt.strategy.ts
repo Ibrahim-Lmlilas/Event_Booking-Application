57984daca862f90ef5d2c537117a520543bfaa3d
Ibrahim-Lmlilas <lmlilas.ibrahim@student.youcode.ma>
2026-02-05 12:21:44 +0100

    EBA-101 API Validation & Error Handling

--- apps/api/src/auth/strategies/jwt.strategy.ts
+++ apps/api/src/auth/strategies/jwt.strategy.ts
@@ -6,6 +6,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from '../../users/schemas/user.schema';
 
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
